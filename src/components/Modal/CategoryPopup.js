dabe3167fda1446987f48e94bb026ddb59ddf24e
Mayur Padshala <mayurpatel.nl@gmail.com>
2023-09-06 16:32:56 +0530

    edit modal dropdown complete

--- src/components/Modal/CategoryPopup.js
+++ src/components/Modal/CategoryPopup.js
@@ -6,13 +6,13 @@ import { toast } from "react-toastify";
 export const CategoryPopup = ({
   modalOpen,
   setModalOpen,
-  onCategoryAdded,
   selectedCategory,
   setSelectedCategory,
 }) => {
   const [dropdownOptions, setDropdownOptions] = useState([]);
   const [selectedOption, setSelectedOption] = useState("");
   const [categoryName, setCategoryName] = useState("");
+  const [isEditingSubcategory, setIsEditingSubcategory] = useState(false); // Add a state to track if editing a subcategory
 
   const token = localStorage.getItem("token");
 
@@ -30,8 +30,12 @@ export const CategoryPopup = ({
         setDropdownOptions(response.data);
         if (selectedCategory) {
           setCategoryName(selectedCategory.name);
+          setIsEditingSubcategory(!!selectedCategory.category_id); // Set isEditingSubcategory based on whether category_id is present
+          setSelectedOption(selectedCategory.category_id || ""); // Set selectedOption based on category_id
         } else {
           setCategoryName("");
+          setIsEditingSubcategory(false);
+          setSelectedOption("");
         }
       } catch (error) {
         console.error("Error fetching dropdown options:", error);
@@ -43,13 +47,9 @@ export const CategoryPopup = ({
   }, [modalOpen, selectedCategory]);
 
   const handleSaveCategory = async () => {
-    const selectedCategoryData = dropdownOptions.find(
-      (option) => option.id === selectedOption
-    );
-
     const requestData = {
       name: categoryName,
-      category_id: selectedCategoryData ? selectedCategoryData.id : null,
+      category_id: isEditingSubcategory ? selectedOption : null, // Use selectedOption when editing subcategory
     };
 
     try {
@@ -63,7 +63,7 @@ export const CategoryPopup = ({
             },
           }
         );
-        toast.success("Category updated successfully!");
+        toast.success(isEditingSubcategory ? "Subcategory updated successfully!" : "Category updated successfully!");
         setSelectedCategory("");
       } else {
         await axios.post(
@@ -75,10 +75,11 @@ export const CategoryPopup = ({
             },
           }
         );
-        toast.success("Category added successfully!");
+        toast.success(isEditingSubcategory ? "Subcategory added successfully!" : "Category added successfully!");
       }
       setCategoryName("");
       setSelectedOption("");
+      setIsEditingSubcategory(false); // Reset isEditingSubcategory
       setModalOpen(false);
     } catch (error) {
       console.error("Error saving category:", error);
@@ -88,11 +89,14 @@ export const CategoryPopup = ({
   const handleCloseModal = () => {
     setSelectedCategory("");
     setCategoryName("");
+    setSelectedOption("");
+    setIsEditingSubcategory(false); // Reset isEditingSubcategory
     setModalOpen(false);
   };
+
   return (
     <Modal
-      title="Add Category and Subcategory"
+      title={isEditingSubcategory ? "Edit Subcategory" : "Add Category"}
       open={modalOpen}
       onCancel={handleCloseModal}
       footer={[
@@ -102,32 +106,51 @@ export const CategoryPopup = ({
           onClick={handleSaveCategory}
           disabled={!categoryName}
         >
-          Add
+          {isEditingSubcategory ? "Save Subcategory" : "Add"}
         </Button>,
       ]}
     >
       <div className="d-flex flex-column">
         <div>
-          <label>Category Name</label>
+          <label>{isEditingSubcategory ? "Subcategory Name" : "Category Name"}</label>
         </div>
         <input
-          placeholder="Category Name"
+          placeholder={isEditingSubcategory ? "Subcategory Name" : "Category Name"}
           value={categoryName}
           onChange={(e) => setCategoryName(e.target.value)}
         />
-        <div className="dropdown mt-3">
-          <select
-            value={selectedOption}
-            onChange={(e) => setSelectedOption(parseInt(e.target.value))}
-          >
-            <option value="">Select a category</option>
-            {dropdownOptions.map((option) => (
-              <option key={option.id} value={option.id}>
-                {option.name}
-              </option>
-            ))}
-          </select>
-        </div>
+        
+        {isEditingSubcategory ? (
+          <div className="mt-3">
+            <div className="dropdown mt-3">
+            <select
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
+              <option value="">Select a category</option>
+              {dropdownOptions.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          </div>
+        ) : (
+          <div className="dropdown mt-3">
+            <select
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
+              <option value="">Select a category</option>
+              {dropdownOptions.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
       </div>
     </Modal>
   );
--- src/components/SubCategory/SubCategory.js
+++ src/components/SubCategory/SubCategory.js
@@ -1,49 +1,62 @@
 import axios from "axios";
 import * as React from "react";
-import {useEffect, useState} from "react"; // Import useEffect
-import {MdDeleteForever, MdEdit} from "react-icons/md";
+import { useEffect, useState } from "react"; // Import useEffect
+import { MdDeleteForever, MdEdit } from "react-icons/md";
 
-export const SubCategoryList = ({categoryId, subcategory}) => {
+export const SubCategoryList = ({ subcategory,editCategory }) => {
+  const [subCategories, setSubCategories] = useState([]);
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    setSubCategories(subcategory);
+  }, [subcategory]);
 
-
-    const [subCategories, setSubCategories] = useState([]);
-    const token = localStorage.getItem("token");
-
-    // Use useEffect to set subCategories when the component mounts
-    useEffect(() => {
-        setSubCategories(subcategory);
-    }, [subcategory]);
-
-    const handleDelete = async (subCategoryId) => {
-        console.log(subCategoryId)
-        try {
-            await axios.delete(`http://192.168.29.12:3000/api/category/${subCategoryId}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-
-            // Update the state after successful deletion
-            setSubCategories((prevSubCategories) =>
-                prevSubCategories.filter((subCategory) => subCategory.id !== subCategoryId)
-            );
-        } catch (error) {
-            console.error("Error deleting sub-category:", error);
+  const handleDelete = async (subCategoryId) => {
+    try {
+      await axios.delete(
+        `http://192.168.29.12:3000/api/category/${subCategoryId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-    };
+      );
+      setSubCategories((prevSubCategories) =>
+        prevSubCategories.filter(
+          (subCategory) => subCategory.id !== subCategoryId
+        )
+      );
+    } catch (error) {
+      console.error("Error deleting sub-category:", error);
+    }
+  };
 
-    return (
-        <div>
-            {subCategories.map((item) => (
-                <div key={item.id}>
-                    <p className='text-dark ms-5 text-start '>{item.name}</p>
-                    <div style={{display: "flex", justifyContent: 'flex-end', position: 'relative', bottom: '25px'}}>
-                        <MdEdit size={18}
-                        />
-                        <MdDeleteForever className='ms-4' onClick={() => handleDelete(item.id)} size={18}/>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div>
+      {subCategories.map((item) => (
+        <div key={item.id}>
+          <p className="text-dark ms-5 text-start ">{item.name}</p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "relative",
+              bottom: "25px",
+            }}
+          >
+            <MdEdit
+              size={18}
+              onClick={() => {
+                editCategory(item.id, item.name,item.category_id);
+              }}
+            />
+            <MdDeleteForever
+              className="ms-4"
+              onClick={() => handleDelete(item.id)}
+              size={18}
+            />
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
